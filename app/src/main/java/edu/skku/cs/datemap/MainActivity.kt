de4d7b803db4055fc97438283f924fc3f30ec477
Kwon Seojin <seojin3154@naver.com>
2024-12-22 12:48:15 +0900

    fix: prevent generating course with empty location

--- app/src/main/java/edu/skku/cs/datemap/MainActivity.kt
+++ app/src/main/java/edu/skku/cs/datemap/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +19,14 @@ class MainActivity : AppCompatActivity() {
 
         generateCourseButton.setOnClickListener {
             val location = locationEditText.text.toString()
-            val intent = Intent(this, LoadingActivity::class.java)
-            intent.putExtra("location", location)
-            startActivity(intent)
-            locationEditText.setText("")
+            if (location.isEmpty()) {
+                Toast.makeText(this, "지역을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, LoadingActivity::class.java)
+                intent.putExtra("location", location)
+                startActivity(intent)
+                locationEditText.setText("")
+            }
         }
 
         viewSavedPlacesButton.setOnClickListener {
