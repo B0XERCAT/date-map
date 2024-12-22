6695613b5bc116777d2b137ef90d568933c8fedb
Kwon Seojin <seojin3154@naver.com>
2024-12-22 12:58:01 +0900

    chore: add ui for empty list view

--- app/src/main/java/edu/skku/cs/datemap/SavedPlacesActivity.kt
+++ app/src/main/java/edu/skku/cs/datemap/SavedPlacesActivity.kt
@@ -1,12 +1,15 @@
 package edu.skku.cs.datemap
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 
 class SavedPlacesActivity : AppCompatActivity() {
     private lateinit var listView: ListView
+    private lateinit var emptyTextView: TextView
     private val savedItems = mutableListOf<DataModel.Item>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +17,17 @@ class SavedPlacesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_saved_places)
 
         listView = findViewById(R.id.placeListView)
+        emptyTextView = findViewById(R.id.emptyTextView)
 
         loadSavedItems()
+
+        if (savedItems.isEmpty()) {
+            emptyTextView.visibility = View.VISIBLE
+            listView.visibility = View.GONE
+        } else {
+            emptyTextView.visibility = View.GONE
+            listView.visibility = View.VISIBLE
+        }
 
         val adapter = ItemAdapter(this, savedItems)
         listView.adapter = adapter
